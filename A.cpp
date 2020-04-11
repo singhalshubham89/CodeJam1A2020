56f9655c6e5ee66e927f03e3ae05df9825991592
Shubham Singhal <singhalshubham89@gmail.com>
2020-04-11 14:25:26 +0530

    Before completiong of coding competition

--- A.cpp
+++ A.cpp
@@ -1,7 +1,44 @@
 #include<bits/stdc++.h>
 using namespace std;
 void solve(){
-
+        vector<string> inp;
+        string temp;
+        string ans;
+        int n;
+        int flag  = 1;
+        cin>>n;
+        if(n > 0){
+            cin>>ans;
+            //ans = ans.substr(1);
+        }
+        for(int i = 1; i  < n; i++){
+            cin>>temp;
+            //temp  = temp.substr(1);
+            int l = ans.size()-1;
+            int k = temp.size()-1;
+            if(flag && ans.size() >= temp.size()){
+                //cout<<ans<<" "<<temp<<endl;
+                while(k >= 0 && flag){
+                    if(temp[k] != ans[l])
+                        flag =0;
+                    k--;
+                    l--;
+                }
+            }else if(flag && ans.size() < temp.size()){
+                 //cout<<ans<<" "<<temp<<endl;
+                 while(l >= 0 && flag){
+                    if(temp[k] != ans[l])
+                        flag =0;
+                    k--;
+                    l--;
+                }
+                ans = temp;
+            }
+        }
+        if(flag)
+            cout<<ans<<endl;
+        else
+            cout<<"*\n";
 }
 int main(){
 
--- B.cpp
+++ B.cpp
@@ -1,15 +1,63 @@
 #include<bits/stdc++.h>
 using namespace std;
-void solve(){
+int binomialCoeff(int n, int k) 
+{ 
+    int res = 1; 
+    if (k > n - k) 
+    k = n - k; 
+    for (int i = 0; i < k; ++i) 
+    { 
+        res *= (n - i); 
+        res /= (i + 1); 
+    } 
+      
+    return res; 
+} 
 
+int path( vector<pair<int,int>> &ans, int rem, int i, int j){
+    if(i >=0 && i <1e9 && j >= 0 && j <= i ){
+        rem -= binomialCoeff(i, j);
+        ans.push_back(make_pair(i+1, j+1));
+        
+        if(rem == 0)
+        {
+            return 1;
+        }else if(rem < 0)
+        {
+            ans.pop_back();
+            return 0;
+        }
+        if( path( ans, rem, i, j+1) ||  path( ans, rem, i+1, j)||path( ans, rem, i+1, j+1) )
+            return 1;
+        else{
+            ans.pop_back();
+            return 0;
+        }
+            
+    }
+    else
+    {
+            return 0;
+    }
+    
 }
+
 int main(){
 
     int t;
     cin>>t;
+   
+
     for(int i = 1; i <= t;i++){
-        cout<<"Case #"<<i<<": ";
-        solve();
+        cout<<"Case #"<<i<<":\n";
+        int sum;
+        vector<pair<int,int>> ans;
+        cin>>sum;
+        int flag = 0;
+        path(ans, sum, 0, 0);
+        for(int j = 0; j < ans.size(); j++){
+            cout<<ans[j].first<<" "<<ans[j].second<<"\n";
+        }
     }
 
 }
